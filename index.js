d092115d27b056f8cbc6fd935f71cb4d0ac32986
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-04-13 17:59:16 -0400

    First working version.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Given Code (Lots of 'Don't worry about it...' in here.)
+
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -7,12 +9,55 @@ function ask(questionText) {
   });
 }
 
+function getRandomInt(min, max) { //  AlTERED this given function so that it runs with MIN and MAX Math
+  return Math.floor(Math.random() * (max - min + 1)) + min; // Rounds DOWN (floor)
+}
+
+
 start();
+
 
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+
+  // My code starts here:
+
+  let max = 100
+  let min = 1
+
+  for (let index = 1; index < 11; index++) {
+    //  This line sets up number of guesses to 3 times for computer (guesser)
+
+    let guess = getRandomInt(min, max) //  Declare variable 'guess' & pass the values from variables min and max to set the range to be guessed (getRandomInt)
+    console.log("My guess is " + guess); //  Print what the computer guesses each time through the for-loop
+    if (guess == secretNumber) {
+      //  first possible outcome (correct guess)
+      console.log("Congratulations, you guessed the right number!");
+      break; //  Stops the loop on correct guess (ending an otherwise endless loop)
+    } else if (guess > secretNumber) {
+      //  second possible outcome (guess too high)
+      console.log("Too high, guess lower.");
+      max = guess - 1 // reassigning the max amount to the current guess value (-1 taking into acct you are eliminating the currently guessed number)
+    } else {
+      //  last (hence 'else') possible outcome (next 2 lines show the actual else-if logic)
+            // } else if (guess < secretNumber) {
+            //   console.log("Too low, guess higher.")
+      console.log("Too low, guess higher.");
+      min = guess + 1 // reassigning the min amount to the current guess value (+1 taking into acct you are eliminating the currently guessed number)
+    }
+  }
+
+  // My code ends here
+
   process.exit();
 }
+
+            // Preliminary Sketching
+            // Guess - Range 0 to 100 (inclusive)
+            // Rounds:
+                // Each turn - 3 possible outcomes:
+                  // Higher
+                  // Lower
+                  // Correct

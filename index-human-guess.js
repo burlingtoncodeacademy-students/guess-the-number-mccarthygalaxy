f00fa2a72a1d50ad2f786f1814afc4f6ae84124c
mccarthygalaxy <124395340+mccarthygalaxy@users.noreply.github.com>
2023-04-17 13:37:59 -0400

    Polished code and comments for turn in.

--- index-human-guess.js
+++ index-human-guess.js
@@ -1,24 +1,12 @@
 
-
-// Legacy code (previous versions): ---------- >
-    // const max_guesses = 0
-    // const min_number = 0
-    // const max_number = 0
-
-    // let min = 1
-    // let max = 100
-    // let guessN = 10
-
-// ^ ------------------------------------------ ^
-
 const readline = require('readline/promises')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-// -------------- Function to generate RND secret number based on range set 
-//          by user (min_number and max_number) below. --------------------------- >
+// -------------- Function to generate random secret number based on range set 
+//        by the user (min_number and max_number) below. --------------------------- >
 
 function getRandInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min 
@@ -38,7 +26,7 @@ async function start () {
     let guesses = 0   
     const secret = getRandInt(min_number, max_number) // Calls (above) function getRandInt to assign Secret Number
 
-// User Customization ------------------------------------------------------->
+// User Customization ---------------------------------------------------------------->
 
   let guessSpan = parseFloat (await rl.question( // Needed to change from CONST so Math.abs & Math.ceil work
     `How many guesses do you require? `)) // declares guessSpan as a num of maximum guesses allowed
@@ -48,10 +36,13 @@ async function start () {
         guessSpan = Math.ceil(guessSpan)    // takes the value and whole integer up    
         console.log(`I rounded you up to ${guessSpan} number of guesses.\n`) 
     } // if number of guesses is proper, no action taken by this if (aka Conditional) statement
-    let max_guesses = guessSpan //  
+    let max_guesses = guessSpan //  assigns user inputed max guesses
     let guessRemain = guessSpan - 1 //  decrements guesses remaining
 
-// ^ User Customization ----------------------------------------------------- ^
+// ^ User Customization -------------------------------------------------------------- ^
+
+
+//  Primary Game Functionality ------------------------------------------------------- >
 
   while (guesses < max_guesses) { //  Keep going if guesses doesn't exceed max guesses
    
@@ -59,7 +50,7 @@ async function start () {
       `Guess a number between ${min_number} and ${max_number}.  `)
     
     if (guess < min_number || guess > max_number) {
-      console.log(`${guess} is a number not in range, please try again. \n`) // Out of range; doesn't decrement number of guesses (no penalty)
+      console.log(`${guess} is a number not in range, please try again. \n`) // doesn't decrement number of guesses (no penalty)
     } else if (guess < secret) {    // if guess LOWER than Secret
       console.log(`${guess} is too low! You have ${guessRemain} guesses remaining. \n`) 
       guesses = guesses + 1     // increments guess count
@@ -74,9 +65,13 @@ async function start () {
       process.exit()    // ends program from inside the WHILE loop
     }
   }
+  
   // Outside the WHILE loop, the conclusion is -> incorrect guesses && ran out of guesses, ergo:
-  console.log(`You failed miserably to guess my secret number - which was, of course, ${secret}. \n`)
+  
+  console.log(`You failed miserably to guess my secret number - which was of course, ${secret}. \n`)
+
+  //  --------------------------------------------------------------------------------- ^
 
   process.exit() // ends program after/outside the WHILE loop
   
-}   // end of async functionality?
+}   // end of async functionality
